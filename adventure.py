ba64715c0f8150307403f4f8edee3dfa8f38f770
alaxander lister <alister18@VenerableBede.schools.sunderland.gov.uk>
2016-11-16 12:15:43 +0000

    finished quiz but gonna spend some time on adventure

--- adventure.py
+++ adventure.py
@@ -1,8 +1,63 @@
 # An adventure game
+health = 10
+sanity = 10
 
-def room1():
+from time import sleep
+def intro():
+    print ("You awake cold, lost and nevous in a dark cell with no memory of your past,")
+    sleep(4)
+    print (" ")
+    print ("Hours pass...")
+    print (" ")
+    sleep(4)
+    print ("Suddenly, and loud 'THUNK' is sounded throughout the mysterious outside of the door,")
+    sleep(3)
+    print ("The door opens, you eyes struggle to ajust to the new light,")
+    sleep(3)
+    print ("You can make out a mysterious figure which is slowly reaching towards you,")
+    sleep(4)
+    print ("The figure grabs you by your raggy shirt and throws you onto a table outside of the cell,")
+    sleep(4)
+    print ("He lifts a rusty, blunt instrument which you still cant make out due to your burning eyes,")
+    sleep(4)
+    print ("A demonic laugh shatters your ears as the man draws closer to you,")
+    sleep(4)
+    print ("You can make out glistening blood, smuthered around the point of the torture weapon,")
+    sleep(4)
+    print ("You know you are in serious trouble and this could be the end,")
+    sleep(4)
+    print ("Your eyes have fully adjusted to the light and you make out a middle aged man, staring at you with a bandana covering his mouth,")
+    sleep(4)
+    print ("You notice a large pile of salt in the corner of your eye, you may be able to reach it...")
+    print (" ")
+    print (" ")
+    sleep(3)
+    choice1()
 
-    ... make a game
+def choice1():
+    global health
+    global sanity
+    print ("A: Grab the salt and throw it into his eyes")
+    sleep(1)
+    print ("B: Roll off the table and run for it")
+    sleep(1)
+    print ("C: Put up a fight and attempt to punch the armed man")
+    answer = input("Pick an option: ")
+    answer = answer.upper()
+    if answer == "A":
+        sleep(1)
+        print ("You decide to Reach for the salt however the vicious man stabs you in the hand mid-action, piercing through the flesh")
+        health = health - 3
+        sleep(4)
+        print ("-3 health")
+        chapter1()
+    elif answer == "B":
+        print ("You make a run for it, barely escaping his grasp, you spot a rusty door and attempt to escape, however it is locked.")
+        sleep(3)
+        print ("The man charges towards you and knocks you to the ground with a heavy punch to your face")
+        health = health - 1
+        sleep(1)
+        print ("-1 health")
 
 
 
@@ -14,4 +69,4 @@ def room1():
 # Leave this at the bottom - it makes room1 run automatically when you
 # run your code.
 if __name__ == "__main__":
-    room1()
+    intro()
--- quiz.py
+++ quiz.py
@@ -1,8 +1,112 @@
 # Our quiz!
 
-def quiz():
+from time import sleep
+respect = 0
+name = ""
 
-    ... make a quiz!
+def quiz():
+    global name
+    
+    print ("what be thy name")
+
+    name = ""
+    for n in range(5):
+        name = input("choose name: ")
+        name = name.title()
+        if name == "Gandalf":
+            print ("YOU AINT MAGIC YOU LIAR, THERES ONLY 1 GANDELF")
+            if n == 4:
+                print("ENOUGH OF YOUR LIES!")
+                quit()
+        else:
+            break
+
+    print ("what is up Comic Sans master " + name + ", im your host, computer source code")
+    print ("lets get right, INTO THE QUIZ")
+    sleep(2)
+    print (" ")
+    Question1()
+
+    
+def Question1():
+    global respect
+    print ("Question 1:")
+    sleep(1)
+    print ("Is comic sans the best font in all of existance ever?")
+    sleep(1)
+    answer = input("Yarp Or Narp?: ")
+    answer = answer.lower()
+    if answer == "yarp":
+        respect = respect + 100
+        print(" + 100 respect for you sir, you damn fine being")
+    elif answer == "narp":
+        respect = respect - 50
+        print("You have just discusted gandalf and gave Donald Trump crippling depression")
+    elif answer == "Lets talk about that...":
+        print("You win all the respects")
+        respect = respect + 1000
+    else:
+        print("lern 2 spel nerd")
+        sleep(3)
+        Question1()
+    Question2()
+
+def Question2():
+    global name
+    global respect
+    print ("Question 2:")
+    sleep(1)
+    print ("Potatoes aint just for eatin'")
+    sleep(1)
+    answer2 = input("True or false: ")
+    answer2 = answer2.lower()
+    if answer2 == "true":
+        print("You have shown great promise " + name)
+        respect = respect + 100
+    elif answer2 == "false":
+        print("you have disappointed Holy Lord Master Gandalf")
+        respect = respect - 50
+    elif answer == "Lets talk about that...":
+        print("You win all the respects")
+        respect = respect + 1000
+    else:
+        print("lern 2 spel nerd")
+        sleep(3)
+        Question2()
+    Question3()
+
+def Question3():
+    global respect
+    print("Question2:")
+    sleep(1)
+    print("Can The Majesctic Awesome Handsome Alex be bothered to make any more questions?")
+    sleep(1)
+    answer3 = input("Nope or Nope: ")
+    answer3 = answer3.lower()
+    if answer3 == "nope":
+        print("yup, you win")
+        respect = respect + 100
+        sleep(1)
+        print("Now get the hell out of my game")
+        print("Respect =", respect)
+        sleep(4)
+        quit()
+    elif answer == "Lets talk about that...":
+        print("You have abused a mighty power", name)
+        respect = respect - 100000000
+        print("respect =", respect)
+    
+    else:
+        print("You will suffer a bloody fate ", name)
+        sleep(1)
+        print("respect = -666")
+        sleep(4)
+        quit()
+        
+        
+        
+    
+    
 
 
 
